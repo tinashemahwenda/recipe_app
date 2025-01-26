dc1808b8b3d9e240803cbd0d6362cb66b8279659
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-26 15:22:40 +0200

    Added more fields

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -1,14 +1,29 @@
 class Recipe {
   String label;
   String imageUrl;
+  int servings;
+  List<Ingredient> ingridients;
 
-  Recipe(this.label, this.imageUrl);
+  Recipe(this.label, this.imageUrl, this.servings, this.ingridients);
 
   static List<Recipe> samples = [
-    Recipe('Spaghetti & Meatballs', 'assets/spaghetti.png'),
-    Recipe('Chocolate Chip Cookies', 'assets/chocolate.png'),
-    Recipe('Tomato Soup', 'assets/tomato.png'),
-    Recipe('Grilled Cheese', 'assets/cheese.png'),
-    Recipe('Pepperoni Pizza', 'assets/pizza.png'),
+    Recipe('Spaghetti & Meatballs', 'assets/spaghetti.png', 4,
+        [Ingredient(1, 'Box', 'Spaghetti')]),
+    Recipe('Chocolate Chip Cookies', 'assets/chocolate.png', 4,
+        [Ingredient(1, 'Box', 'Spaghetti')]),
+    Recipe('Tomato Soup', 'assets/tomato.png', 4,
+        [Ingredient(1, 'Box', 'Spaghetti')]),
+    Recipe('Grilled Cheese', 'assets/cheese.png', 4,
+        [Ingredient(1, 'Box', 'Spaghetti')]),
+    Recipe('Pepperoni Pizza', 'assets/pizza.png', 4,
+        [Ingredient(1, 'Box', 'Spaghetti')]),
   ];
+}
+
+class Ingredient {
+  double quantity;
+  String measure;
+  String name;
+
+  Ingredient(this.quantity, this.measure, this.name);
 }
