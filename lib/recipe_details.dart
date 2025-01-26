bee25659319a149a8bf7eaa20284b12458d7172f
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-26 15:11:38 +0200

    Created recipe details page

---
+++ lib/recipe_details.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'recipe.dart';
+
+class RecipeDetails extends StatefulWidget {
+  final Recipe recipe;
+  const RecipeDetails({super.key, required this.recipe});
+
+  @override
+  State<RecipeDetails> createState() => _RecipeDetailsState();
+}
+
+class _RecipeDetailsState extends State<RecipeDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.recipe.label),
+      ),
+    );
+  }
+}
