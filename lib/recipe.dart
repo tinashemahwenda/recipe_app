bdc0d2a22ed5f066ce779c57327721a7d2389291
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-26 14:29:28 +0200

    Added assets

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -3,4 +3,12 @@ class Recipe {
   String imageUrl;
 
   Recipe(this.label, this.imageUrl);
+
+  static List<Recipe> samples = [
+    Recipe('Spaghetti & Meatballs', 'assets/spagheti.jpg'),
+    Recipe('Chocolate Chip Cookies', 'assets/chocolate.jpg'),
+    Recipe('Tomato Soup', 'assets/tomato.jpg'),
+    Recipe('Grilled Cheese', 'assets/cheese.jpg'),
+    Recipe('Pepperoni Pizza', 'assets/pizza.jpg'),
+  ];
 }
